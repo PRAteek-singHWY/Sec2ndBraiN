01c1bbd4cdb362c74aafc1bc63f76be46b7fc9de
PRAteek-singHWY <prateek23022004@gmail.com>
2025-10-25 19:53:28 +0530

    final amends

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,4 @@
 // client/src/config.ts
 // This correctly defaults to localhost:3000 when VITE_API_URL isn't set (locally)
 export const BACKEND_URL =
-  // import.meta.env.VITE_API_URL || 
-  "http://localhost:3000";
+  import.meta.env.VITE_API_URL || "http://localhost:3000";
